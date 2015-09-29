b8a9426876be0fb1d42cd568d658a32dd10cedb2
Simon Woolf <simon@ably.io>
2015-09-29 21:27:55 +0100

    Mailbox: introduce defunct state

--- ruby/mailbox.rb
+++ ruby/mailbox.rb
@@ -1,6 +1,7 @@
 class Mailbox
   def initialize(socket)
     @socket = socket
+    @defunct = false
   end
 
   # max amount of data that can be pushed on to a unix socket in a DGRAM packet
@@ -9,13 +10,17 @@ class Mailbox
   # collate, just make sure all packets are under the size limit.
   #
   # max size of dgram packet is around 212000
-  def send(message)
+  def sendmsg(message)
     begin
       @socket.sendmsg_nonblock Marshal.dump(message), 0
     rescue IO::EAGAINWaitWritable
       # socket is full
       # TODO put the data into a queue rather than giving up
       raise RuntimeError, "socket is full"
+    rescue Errno::EPIPE
+      # The other socket is closed, probably because the event loop's stopped. clean up
+      b.close
+      @defunct = true
     end
   end
 
@@ -27,6 +32,10 @@ class Mailbox
       # nothing there
       nil
     end
+  end
+
+  def defunct?
+    @defunct
   end
 end
 
